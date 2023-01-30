a2d6ed27cad8277a62669cddb4cc3861ed5f914f
mabouzeid04 <mabouzeid120@gmail.com>
2023-01-29 19:38:53 -0800

    bug fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void myTestReverseInPlace() {
+    int[] input1 = { 3, 2, 1, 0 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 0, 1, 2, 3}, input1);
+	}
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void myTestReversed() {
+    int[] input1 = { 3, 2, 1, 0 };
+    assertArrayEquals(new int[]{ 0, 1, 2, 3 }, ArrayExamples.reversed(input1));
   }
 }
